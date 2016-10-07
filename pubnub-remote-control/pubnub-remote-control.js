24c8e923e1195653d46e0e33ef7a474308d52875
Salvatore Cordiano <github@parallel.it>
2016-10-07 23:13:29 +0200

    Change method visibility

--- pubnub-remote-control/pubnub-remote-control.js
+++ pubnub-remote-control/pubnub-remote-control.js
@@ -10,54 +10,54 @@
 
     'use strict';
 
-    var options, pubnub;
+    var options;
+
     var defaultProperties = {
-        publishKey: null,
         subscribeKey: null,
         inputChannel: 'input'
     };
 
     var PubnubRemoteControl = function PubnubRemoteControl(customProperties) {
         if (customProperties && typeof customProperties === "object") {
-            this.options = this.extendDefaultProperties(defaultProperties, customProperties);
+            this.options = extendDefaultProperties(defaultProperties, customProperties);
         }
-        this.initPubnub(this.options.subscribeKey, this.options.inputChannel);
+        initPubnub(this.options.subscribeKey, this.options.inputChannel);
     }
 
-    PubnubRemoteControl.prototype = {
-        initPubnub: function initPubnub(subscribeKey, inputChannel) {
-            pubnub = PUBNUB.init({ subscribe_key: subscribeKey, ssl: (('https:' == document.location.protocol) ? true : false) });
-            pubnub.subscribe({ channel: inputChannel, message: this.processInput });
-        },
-        processInput: function processInput(input) {
-            if(input && typeof input === "object" && input.button) {
-                switch (input.button) {
-                    case 'left' :
-                        Reveal.navigateLeft();
-                        break;
-                    case 'right' :
-                        Reveal.navigateRight();
-                        break;
-                    case 'up' :
-                        Reveal.navigateUp();
-                        break;
-                    case 'down' :
-                        Reveal.navigateDown();
-                        break;
-                }
+    function initPubnub(subscribeKey, inputChannel) {
+        var pubnub = PUBNUB.init({ subscribe_key: subscribeKey, ssl: (('https:' == document.location.protocol) ? true : false) });
+        pubnub.subscribe({ channel: inputChannel, message: processInput });
+    }
+
+    function processInput(input) {
+        if(input && typeof input === "object" && input.button) {
+            switch (input.button) {
+                case 'left' :
+                    Reveal.navigateLeft();
+                    break;
+                case 'right' :
+                    Reveal.navigateRight();
+                    break;
+                case 'up' :
+                    Reveal.navigateUp();
+                    break;
+                case 'down' :
+                    Reveal.navigateDown();
+                    break;
             }
-        },
-        extendDefaultProperties: function extendDefaultProperties(defaultProperties, customProperties) {
-            var property;
-            for (property in customProperties) {
-                if (customProperties.hasOwnProperty(property)) {
-                    defaultProperties[property] = customProperties[property];
-                }
-            }
-            return defaultProperties;
         }
     }
-    
+
+    function extendDefaultProperties(defaultProperties, customProperties) {
+        var property;
+        for (property in customProperties) {
+            if (customProperties.hasOwnProperty(property)) {
+                defaultProperties[property] = customProperties[property];
+            }
+        }
+        return defaultProperties;
+    }
+
     Reveal.getConfig().pubnubRemoteControl && new PubnubRemoteControl(Reveal.getConfig().pubnubRemoteControl);
 
 }());
