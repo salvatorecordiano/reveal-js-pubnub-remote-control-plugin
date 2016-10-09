644e1a49ef0806a0a7bbdfba4adbd3c3c44a8c40
Salvatore Cordiano <github@parallel.it>
2016-10-09 16:42:28 +0200

    Added Telegram Bot Remote Control

---
+++ remote-control-telegram-bot/configuration.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'publishKey' => '---INSERT KEY HERE---',
+    'subscribeKey' => '---INSERT KEY HERE---',
+    'inputChannel' => 'input',
+    'allowedUsernames' => ['---INSERT USERNAMES HERE---'],
+];
---
+++ remote-control-telegram-bot/webhook.php
@@ -0,0 +1,51 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+$configuration = require_once 'configuration.php';
+
+$pubnub = new Pubnub\Pubnub($configuration['publishKey'], $configuration['subscribeKey']);
+
+$content = file_get_contents('php://input');
+$update = json_decode($content, true);
+
+if(!$update)
+{
+    exit;
+}
+
+$message = isset($update['message']) ? $update['message'] : '';
+$messageId = isset($message['message_id']) ? $message['message_id'] : '';
+$chatId = isset($message['chat']['id']) ? $message['chat']['id'] : '';
+$senderId = isset($message['chat']['id']) ? $message['chat']['id'] : '';
+$username = isset($message['chat']['username']) ? $message['chat']['username'] : '';
+$text = isset($message['text']) ? $message['text'] : "";
+
+$allowedActions = ['left', 'right', 'up', 'down'];
+
+if(!in_array($username, $configuration['allowedUsernames'])) {
+    exit;
+}
+
+$text = trim($text);
+$text = strtolower($text);
+header('Content-Type: application/json');
+
+$response = '';
+
+if(strpos($text, "/start") === 0)
+{
+    $response = 'Welcome to reveal.js Remote Control Bot';
+}
+else if($text && in_array($text, $allowedActions)) {
+    $response = "@$username: " . $text;
+    $pubnub->publish($configuration['inputChannel'], ['button' => $text]);
+} else {
+    $response = "Wrong message";
+}
+
+$parameters = [];
+$parameters["chat_id"] = $chatId;
+$parameters["text"] = $response;
+$keyboard = ['keyboard' => [['LEFT'], ['RIGHT'], ['UP'], ['DOWN']], 'one_time_keyboard' => false];
+$parameters["reply_markup"] = json_encode($keyboard, true);
+$parameters["method"] = "sendMessage";
+echo json_encode($parameters);
